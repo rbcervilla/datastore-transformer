f33f32b828c75442f77133c23360a9739a6c131d
Ruben Cervilla <rbcervilla@gmail.com>
2022-11-14 16:54:48 +0100

    Transformer can return an error

--- transformer.go
+++ transformer.go
@@ -6,32 +6,34 @@ import "cloud.google.com/go/datastore"
 //
 // Transform performs transformations on the Entity
 type Transformer interface {
-	Transform(e *Entity)
+	Transform(e *Entity) error
 }
 
 // TransformerFunc type is an adapter to allow the use of functions
 // as Transformer
-type TransformerFunc func(e *Entity)
+type TransformerFunc func(e *Entity) error
 
-func (f TransformerFunc) Transform(e *Entity) {
-	f(e)
+func (f TransformerFunc) Transform(e *Entity) error {
+	return f(e)
 }
 
 // RemoveField returns a TransformerFunc that remove field of the Entity
 func RemoveField(field string) TransformerFunc {
-	return func(e *Entity) {
+	return func(e *Entity) error {
 		delete(e.Properties, field)
+		return nil
 	}
 }
 
 // SetField returns a TransformerFunc that sets a field to the Entity
 func SetField(field string, value interface{}, index bool) TransformerFunc {
-	return func(e *Entity) {
+	return func(e *Entity) error {
 		f := datastore.Property{
 			Name:    field,
 			Value:   value,
 			NoIndex: !index,
 		}
 		e.Properties[field] = f
+		return nil
 	}
 }
